8686d802c0d43d2b36c494907c5c13461dcb5701
DanielleT83 <tylman.danielle@gmail.com>
2026-01-30 20:33:19 -0600

    updated some assets and added a tutorial

--- scripts/level_select.gd
+++ scripts/level_select.gd
@@ -73,3 +73,6 @@ func _on_button_pressed(level: int):
 	level -= 1
 	Global.current_level_data = levels[level]
 	get_tree().change_scene_to_file("res://scenes/level.tscn")
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/title screen.tscn")
--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -1,11 +1,11 @@
 extends Control
 
-@onready var button:TextureButton = $StartButton
-
+@onready var startButton:TextureButton = $StartButton
+@onready var tutButton:TextureButton = $TutorialButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$TutorialScreen.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,4 +13,12 @@ func _process(_delta: float) -> void:
 	pass
 
 func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level select.tscn")
+
+func _on_tutorial_button_pressed() -> void:
+	$TutorialScreen.show()
+	startButton.disabled = true
+	tutButton.disabled = true
+
+func _on_proceed_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level select.tscn")
